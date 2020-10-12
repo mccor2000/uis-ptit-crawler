ba81ad594c972affd6fcab1e6ef9f39a45c4b079
mccor2000 <roccm0200@gmail.com>
2020-10-12 22:48:30 +0700

    feat: add regularClass model

---
+++ src/models/regularClass.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+export const RegularClassSchema = new Schema({
+  classID: {
+    type: String,
+    required: true,
+  },
+  className: {
+    type: String,
+  },
+  students: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "student",
+    },
+  ],
+});
+
+export const RegularClass = model(
+  "regular_class",
+  RegularClassSchema,
+  "regular_classes"
+);
