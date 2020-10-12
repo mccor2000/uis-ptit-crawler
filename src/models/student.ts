b2316ce96a708cebe292894685a87b12fda746e9
mccor2000 <roccm0200@gmail.com>
2020-10-12 22:48:43 +0700

    feat: add student model

---
+++ src/models/student.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+export const StudentSchema = new Schema({
+  studentID: {
+    type: String,
+    required: true,
+  },
+
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  classID: {
+    type: String,
+    required: true,
+  },
+
+  joinedClasses: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "subject",
+    },
+  ],
+});
+
+export const StudentModel = model("student", StudentSchema, "students");
