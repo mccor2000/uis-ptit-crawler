5bdb36339fa825dd88456762e02bbd668d8bc1cd
mccor2000 <roccm0200@gmail.com>
2020-10-14 11:56:24 +0700

    fix: change mongoose _id field to default, separate studentId, classId field

--- src/models/creditClass.ts
+++ src/models/creditClass.ts
@@ -1,11 +1,40 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
+
+// import { StudentModel } from "./student";
+
+export interface CreditClass extends Document {
+  classId: string;
+  subjectId: string;
+  subjectTitle: string;
+  group: string;
+  credits: string;
+  classes: [string];
+  schedule: [
+    {
+      day: string;
+      room: string;
+      startTime: string;
+      duration: string;
+      startDate: string;
+      endDate: string;
+    }
+  ];
+  students: [
+    {
+      studentId: string;
+      firstName: string;
+      lastName: string;
+      classId: string;
+    }
+  ];
+}
 
 export const CreditClassSchema = new Schema({
-  _id: {
+  classId: {
     type: String,
     required: true,
   },
-  subjectCode: {
+  subjectId: {
     type: String,
     required: true,
   },
@@ -20,7 +49,7 @@ export const CreditClassSchema = new Schema({
     required: true,
   },
 
-  credit: {
+  credits: {
     type: String,
     required: true,
   },
@@ -38,10 +67,30 @@ export const CreditClassSchema = new Schema({
     },
   ],
 
-  students: Array,
+  students: [
+    {
+      studentId: String,
+      firstName: String,
+      lastName: String,
+      classId: String,
+    },
+  ],
 });
 
-export const CreditClass = model(
+// CreditClassSchema.pre("save", async function () {
+// const currentValue = (this as CreditClass).students;
+//
+// const register = currentValue.map((student) => {
+// StudentModel.findOneAndUpdate(
+// { studentId: student.studentId },
+// { $push: { joinedClasses: this._id } }
+// );
+// });
+//
+// await Promise.all(register);
+// });
+
+export const CreditClassModel = model(
   "credit_class",
   CreditClassSchema,
   "credit_classes"
